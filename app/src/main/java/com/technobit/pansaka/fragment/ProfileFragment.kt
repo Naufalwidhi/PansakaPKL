5f88485c75d83c60a554bffec8f324c1e8623f52
Lazuardi <lazuardi490@gmail.com>
2021-08-06 23:54:15 +0700

    Profile Done

--- app/src/main/java/com/technobit/pansaka/fragment/ProfileFragment.kt
+++ app/src/main/java/com/technobit/pansaka/fragment/ProfileFragment.kt
@@ -2,24 +2,35 @@ package com.technobit.pansaka.fragment
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.technobit.pansaka.R
 import com.technobit.pansaka.activity.ChangePasswordActivity
+import com.technobit.pansaka.api.Client
+import com.technobit.pansaka.api.constant
+import com.technobit.pansaka.model.PrefsToken
+import com.technobit.pansaka.model.Profile
+import com.technobit.pansaka.model.ProfileResponse
 import kotlinx.android.synthetic.main.fragment_profile.*
-import kotlinx.android.synthetic.main.fragment_transaksi.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class ProfileFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private val myPreftoken by lazy { PrefsToken(this.requireContext()) }
+    private lateinit var profile: Profile
+    private lateinit var token: String
+    private lateinit var name : TextView
+    private lateinit var image : ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,7 +46,11 @@ class ProfileFragment : Fragment() {
         (activity as AppCompatActivity).setSupportActionBar(toolbar_profile)
         (activity as AppCompatActivity).supportActionBar?.title = "Profile"
 
+        name = view.findViewById(R.id.profile_name)
+        image = view.findViewById(R.id.img_profile)
+
         setChangePassButton()
+        loadProfile()
     }
 
     private fun setChangePassButton(){
@@ -43,5 +58,38 @@ class ProfileFragment : Fragment() {
             val intent = Intent(context,ChangePasswordActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun loadProfile() {
+        token = "Bearer " + myPreftoken.getusertoken()
+        Client.myApiClient()
+            .profile(constant.appId, constant.key, token)
+            .enqueue(object : Callback<ProfileResponse> {
+                override fun onResponse(
+                    call: Call<ProfileResponse>,
+                    response: Response<ProfileResponse>
+                ) {
+
+                    if (response.isSuccessful) {
+                        response.body()?.let {
+                            val name_profile = it.data[0].nameprofile
+
+                            profile_name?.text = name_profile
+                            img_profile.apply {
+                                context?.let { it1 ->
+                                    Glide.with(it1)
+                                        .load(it.data[0].profilepic)
+                                        .into(this)
+                                }
+                            }
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<ProfileResponse>, t: Throwable) {
+                    Toast.makeText(context, t.message, Toast.LENGTH_SHORT).show()
+                }
+
+            })
     }
 }
